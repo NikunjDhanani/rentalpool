9be368e75ce4d42a6068426f192668c04bd01e64
Krunal5001 <krunalbeladiya11@gmail.com>
2024-05-08 13:27:50 +0530

    chat box page set ui

--- src/app/pages/chatbox/page.js
+++ src/app/pages/chatbox/page.js
@@ -6,18 +6,29 @@ const ChatBox = () => {
 
     return (
         <div className="container">
-            <div className="row gx-2">
+            <div className="row gx-2 mt-3">
                 <div className="contactus-container col-3">
                     <h5 className="fw-bold">Chat Box</h5>
                 </div>
                 <div className="contactus-container col-9">
-                    <div className="d-flex">
-                        <span className="user-name">Divya Patel</span>
-                        <span>
-                            <div className="product-name">Black slik dotted...</div>
-                            <div>$ 1500/day</div>
-                        </span>
-                        <button className="btn btn-request-review">Request Review</button>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div>
+                            <img className="user-image" src="/assets/userImage.png" alt="User" />
+                            <span className="user-name">Divya Patel</span>
+                        </div>
+                        <div className="d-flex">
+                            <span>
+                                <div className="product-name">Black slik dotted...</div>
+                                <span className="product-amount">$ 1500/</span><span className="rent-day">day</span>
+                            </span>
+                            <button className="btn btn-request-review">Request Review</button>
+                        </div>
+                    </div>
+                    <div className="chat-box">
+                        <div className="chat-part">
+                            <div className="incomeing-message">Lorem ipsum dolor sit amet</div>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad.</div>
+                        </div>
                     </div>
                 </div>
             </div>
