cfbf43a65c55d5d6144fe8a61e7969528e8775e8
Krunal5001 <krunalbeladiya11@gmail.com>
2024-05-09 19:21:42 +0530

    chat box in user details set

--- src/app/pages/chatbox/page.js
+++ src/app/pages/chatbox/page.js
@@ -1,7 +1,43 @@
 "use client"
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ChatBox = () => {
+    const [senderMessageDetails, setSenderMessageDetails] = useState([]);
+    const [senderUserDerails, setSenderUserDerails] = useState([]);
+
+    const authToken = localStorage.getItem("authToken");
+
+    useEffect(() => {
+        axios({
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}products/myInquiries/`,
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: `Token ${authToken}`,
+            },
+        })
+            .then((res) => {
+                setSenderMessageDetails(res.data)
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, []);
+
+    // Function to format date and time
+    const formatDateTime = (dateTimeString) => {
+        const date = new Date(dateTimeString);
+        return date.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: '2-digit',
+            hour12: true
+        });
+    };
+
+    const handleSenerMessageDetails = (data) => {
+        setSenderUserDerails(data);
+    }
 
     return (
         <div className="container-lg">
@@ -9,32 +45,36 @@ const ChatBox = () => {
                 <div className="contactus-container p-0 message-sender-side">
                     <h5 className="chat-box-header-name">Chat Box</h5>
                     <div className="message-sender-list">
-                        <div className="message-sender d-flex justify-content-between align-items-center px-2 py-1">
-                            <div className="d-flex align-items-center">
-                                <img className="user-image" src="/assets/userImage.png" alt="User" />
-                                <div className="px-2">
-                                    <div className="user-name-sender">Divya Patel</div>
-                                    <div className="product-name-sender">Blue Demin Jacket</div>
+                        {senderMessageDetails && senderMessageDetails.map((data) => (
+                            <div key={data.id} className="message-sender d-flex justify-content-between align-items-center px-2 py-1" onClick={() => handleSenerMessageDetails(data)}>
+                                {console.log(data, 'datadatadata')}
+                                <div className="d-flex align-items-center">
+                                    <img className="user-image" src="/assets/userImage.png" alt="User" />
+                                    <div className="px-2">
+                                        <div className="user-name-sender">{data.buyer.name}</div>
+                                        <div className="product-name-sender">{data.product.title}</div>
+                                    </div>
+                                </div>
+                                <div className="text-center">
+                                    <div className="message-time">{formatDateTime(data.updated_at)}</div>
+                                    {data.unread_messages !== 0 &&
+                                        <div>
+                                            <div className="total-send-message d-flex justify-content-center align-items-center">{data.unread_messages > 100 ? '99+' : data.unread_messages}</div>
+                                        </div>}
                                 </div>
                             </div>
-                            <div className="text-center">
-                                <div className="message-time">12:25 PM</div>
-                                <div>
-                                    <div className="total-send-message d-flex justify-content-center align-items-center">99+</div>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
                 <div className="contactus-container chat-box-side">
                     <div className="d-flex justify-content-between align-items-center chat-box-header">
                         <div>
                             <img className="user-image" src="/assets/userImage.png" alt="User" />
-                            <span className="user-name">Divya Patel</span>
+                            <span className="user-name">{senderUserDerails?.buyer?.name}</span>
                         </div>
                         <div className="d-flex">
                             <span>
-                                <div className="product-name">Black slik dotted...</div>
+                                <div className="product-name">{senderUserDerails?.product?.title}</div>
                                 <span className="product-amount">$ 1500/</span><span className="rent-day">day</span>
                             </span>
                             <button className="btn btn-request-review">Request Review</button>
